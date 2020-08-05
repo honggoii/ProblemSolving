b13ea80ca14c49ed10bd856b1f097d2f7a91f433
honggoii <46019755+honggoii@users.noreply.github.com>
2020-08-05 10:08:57 +0900

    Create 10845.cpp

---
+++ 20200805/10845.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int arr[10001];
+char ch[101];
+int head, tail;
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	while (n--) {
+		cin >> ch;
+		if (!strcmp(ch, "push")) {
+			int a;
+			cin >> a;
+			arr[tail++] = a;
+		}
+		else if (!strcmp(ch, "pop")) {
+			if (head != tail) {
+				cout << arr[head] << '\n';
+				head++;
+			}
+			else {
+				cout << "-1" << '\n';
+			}
+		}
+		else if (!strcmp(ch, "size")) {
+			cout << tail - head << '\n';
+		}
+		else if (!strcmp(ch, "empty")) {
+			if (head == tail)
+				cout << "1" << '\n';
+			else
+				cout << "0" << '\n';
+		}
+		else if (!strcmp(ch, "front")) {
+			if (head == tail)
+				cout << "-1" << '\n';
+			else
+				cout << arr[head] << '\n';
+		}
+		else if (!strcmp(ch, "back")) {
+			if (head == tail)
+				cout << "-1" << '\n';
+			else
+				cout << arr[tail - 1] << '\n';
+		}
+	}
+	return 0;
+}
