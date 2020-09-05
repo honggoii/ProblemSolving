6040bc4f57e9d1b00dcf735a9168ef02bd8169bf
honggoii <46019755+honggoii@users.noreply.github.com>
2020-09-05 18:32:59 +0900

    Update 10866.cpp

--- 20200905/10866.cpp
+++ 20200905/10866.cpp
@@ -3,23 +3,22 @@
 using namespace std;
 deque<int> dq;
 int main() {
-	freopen("sample.txt", "r", stdin);
 	int tc;
 	cin >> tc;
 	while (tc--) {
 		string s;
 		cin >> s;
-		if (s == "push_front") {
+		if (!s.compare("push_front")) {
 			int a;
 			cin >> a;
 			dq.push_front(a);
 		}
-		else if (s == "push_back") {
+		else if (!s.compare("push_back")) {
 			int a;
 			cin >> a;
 			dq.push_back(a);
 		}
-		else if (s == "pop_front") {
+		else if (!s.compare("pop_front")) {
 			if (dq.empty())
 				cout << "-1" << "\n";
 			else {
@@ -27,7 +26,7 @@ int main() {
 				dq.pop_front();
 			}
 		}
-		else if (s == "pop_back") {
+		else if (!s.compare("pop_back")) {
 			if (dq.empty())
 				cout << "-1" << "\n";
 			else {
@@ -35,22 +34,22 @@ int main() {
 				dq.pop_back();
 			}
 		}
-		else if (s == "size") {
+		else if (!s.compare("size")) {
 			cout << dq.size() << "\n";
 		}
-		else if (s == "empty") {
+		else if (!s.compare("empty")) {
 			if (dq.empty())
 				cout << "1" << "\n";
 			else
 				cout << "0" << "\n";
 		}
-		else if (s == "front") {
+		else if (!s.compare("front")) {
 			if (dq.empty())
 				cout << "-1" << "\n";
 			else
 				cout << dq.front() << "\n";
 		}
-		else if (s == "back") {
+		else if (!s.compare("back")) {
 			if (dq.empty())
 				cout << "-1" << "\n";
 			else
