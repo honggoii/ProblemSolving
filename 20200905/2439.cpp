25f8285929cc5d58353294c6df4c7d2fec4fa9e1
honggoii <46019755+honggoii@users.noreply.github.com>
2020-09-05 15:18:49 +0900

    Create 2439.cpp

---
+++ 20200905/2439.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+int main() {
+	int a;
+	scanf("%d", &a);
+	for (int i = 1; i <= a; i++) {
+		for (int j = 1; j <= a - i; j++)
+			printf(" ");
+		for (int j = 1; j <= i; j++)
+			printf("*");
+		printf("\n");
+	}
+	return 0;
+}
