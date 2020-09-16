153de2243edb2f116f469db4a25bcaac85d34ac9
honggoii <46019755+honggoii@users.noreply.github.com>
2020-09-16 23:59:43 +0900

    Create 15655.cpp

---
+++ 20200916/15655.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+int n, m;
+int temp[20];
+int arr[20];
+bool visit[20];
+void comb(int idx, int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < cnt; i++) {
+			printf("%d ", arr[i]);
+		}
+		printf("\n");
+		return;
+	}
+
+	for (int i = idx; i < n; i++) {
+		if (visit[i]) continue;
+		visit[i] = true;
+		arr[cnt] = temp[i];
+		comb(i, cnt + 1);
+		visit[i] = false;
+	}
+}
+int main() {
+	scanf("%d%d", &n, &m);
+	for (int i = 0; i < n; i++)
+		scanf("%d", &temp[i]);
+	sort(temp, temp + n);
+	comb(0, 0);
+	return 0;
+}
