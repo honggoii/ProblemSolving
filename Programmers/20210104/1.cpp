bf5c45c2651578d94ddf08bfa1d7442b7206b21c
honggoii <46019755+honggoii@users.noreply.github.com>
2021-01-04 11:29:44 +0900

    위장

---
+++ Programmers/20210104/1.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 1;
+    map<string, int> m;
+    map<string, int>:: iterator it;
+
+    for (int i = 0; i < clothes.size(); i++) {
+        m[clothes[i][1]]++;
+    }
+
+    for (auto c : m) {
+        answer *= (c.second + 1);
+    }
+
+    answer--;
+    return answer;
+}
