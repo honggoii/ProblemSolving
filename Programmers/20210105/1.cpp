b2836ff73607b8d0ff919014d365b37f3691e68b
honggoii <46019755+honggoii@users.noreply.github.com>
2021-01-05 10:15:10 +0900

    가장 큰 수

---
+++ Programmers/20210105/1.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool cmp(const pair<int, string>& a, const pair<int, string>& b) {
+    return a.second > b.second;
+}
+
+string solution(vector<int> numbers) {
+    string answer = "";
+    vector<pair<int, string>> temp;
+
+    for (int i = 0; i < numbers.size(); i++) {
+        string num = to_string(numbers[i]);
+        while (num.size() <= 4)
+            num += num;
+        temp.push_back(make_pair(i, num));
+    }
+
+    sort(temp.begin(), temp.end(), cmp);
+
+    if (numbers[temp[0].first] == 0) {
+        answer = to_string(0);
+    } 
+    else {
+        for (int i = 0; i < temp.size(); i++) {
+            answer += to_string(numbers[temp[i].first]);
+        }
+    }           
+
+    return answer;
+}
