d3e1e81482f73c2cb10b6eb0cc4ef6e62ba2a136
honggoii <46019755+honggoii@users.noreply.github.com>
2021-01-06 10:28:22 +0900

    124 나라의 숫자

---
+++ Programmers/20210106/1.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    int arr[3] = { 4,1,2 };
+    //arr[0] = 4
+    //arr[1] = 1
+    //arr[2] = 2
+    
+    while (n != 0) {
+        answer += to_string(arr[n % 3]);
+        if (n % 3 == 0)
+            n = (n/3) - 1;
+        else
+            n /= 3;
+    }
+
+    reverse(answer.begin(), answer.end());
+
+    return answer;
+}
