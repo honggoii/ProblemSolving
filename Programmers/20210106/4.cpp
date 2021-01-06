a4a8d6b86a69adb35830822248ec15aa69378abf
honggoii <46019755+honggoii@users.noreply.github.com>
2021-01-06 12:01:52 +0900

    스킬트리

---
+++ Programmers/20210106/4.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string skill, vector<string> skill_trees) {
+    int answer = 0;
+    for (int i = 0; i < skill_trees.size(); i++) {
+        bool flag = false;
+        int now = 0;
+        for (int j = 0; j < skill.size(); j++) {
+            for (int k = 0; k < skill_trees[i].size(); k++) {
+                if (skill[j] == skill_trees[i][k]) {
+                    //같은 스킬 발견
+                    if (k < now) {
+                        //선행 스킬을 무시한 경우
+                        flag = true;
+                    }
+                    else {
+                        now = k;//현재 인덱스 저장
+                    }
+                    break;
+                }
+                else {
+                    //스킬 발견하지 못했는데 스킬 트리 끝까지 갔으면
+                    if (k == skill_trees[i].size() - 1) {
+                        now = skill_trees[i].size();
+                    }
+                }
+            }
+            if (flag)
+                break;
+        }
+        if (!flag)
+            answer++;
+    }
+    return answer;
+}
