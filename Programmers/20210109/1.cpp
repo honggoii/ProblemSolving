2d8d7f9492e7a3f6a1f08bfcbe1d9797f9de9126
honggoii <46019755+honggoii@users.noreply.github.com>
2021-01-09 19:51:17 +0900

    주식가격

--- Programmers/20210109/1.cpp
+++ Programmers/20210109/1.cpp
@@ -6,29 +6,20 @@
 using namespace std;
 
 deque<pair<int, int>> dq;//(값, 인덱스)
-vector<pair<int, int>> v;//(인덱스, 답)
-
-bool cmp(pair<int, int> a, pair<int, int> b) {
-    return a.first < b.first;
-}
 
 vector<int> solution(vector<int> prices) {
-    vector<int> answer;
+    vector<int> answer(prices.size());
     dq.push_back(make_pair(prices[0], 0));
     for (int i = 1; i < prices.size(); i++) {
         while (!dq.empty() && (dq.back().first > prices[i])) {
-            v.push_back(make_pair(dq.back().second, i - dq.back().second));
+            answer[dq.back().second] = i - dq.back().second;
             dq.pop_back();
         }
         dq.push_back(make_pair(prices[i], i));
     }
     while (!dq.empty()) {
-        v.push_back(make_pair(dq.front().second, prices.size() - 1 - dq.front().second));
+        answer[dq.front().second] = prices.size() - 1 - dq.front().second;
         dq.pop_front();
-    }
-    sort(v.begin(), v.end(), cmp);
-    for (int i = 0; i < v.size(); i++) {
-        answer.push_back(v[i].second);
     }
     return answer;
 }
