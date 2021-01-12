782d32056848521ea5ca9c2ebb494b2288c5b50b
honggoii <46019755+honggoii@users.noreply.github.com>
2021-01-12 14:53:39 +0900

    더 맵게

---
+++ Programmers/20210112/1.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+priority_queue<int, vector<int>, greater<int>> pq;
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+    int sz = scoville.size();
+    if (sz == 1 && K >= 2)
+        answer = -1;
+    else {
+        for (int i = 0; i < sz; i++)
+            pq.push(scoville[i]);//우선순위 큐에 넣는다.
+
+        while (1) {
+            int temp1 = pq.top(); pq.pop();
+            int temp2 = 0;
+            if (pq.empty()) {
+                answer = -1;
+                break;
+            }
+            else {
+                temp2 = pq.top(); pq.pop();
+                int temp = temp1 + (temp2 * 2);//공식
+                pq.push(temp);//결과 넣기
+                answer++;
+            }              
+
+            if (pq.top() >= K)
+                break;
+        }
+    }
+    return answer;
+}
