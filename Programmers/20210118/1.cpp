97b8bebcad0c2f52a38903fc49109492e01700b0
honggoii <46019755+honggoii@users.noreply.github.com>
2021-01-18 16:15:00 +0900

    올바른 괄호

---
+++ Programmers/20210118/1.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    int sz = s.size();
+    stack<char> st;
+
+    for (int i = 0; i < sz; i++) {
+        if (s[i] == '(')
+            st.push(s[i]);
+        else if (s[i] == ')') {
+            if (st.empty()) {
+                answer = false;
+                break;
+            }
+            st.pop();
+        }
+    }
+
+    if (!st.empty())
+        answer = false;
+
+    return answer;
+}
