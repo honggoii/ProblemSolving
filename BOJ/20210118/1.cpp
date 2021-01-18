b7b5cd65447f14790d42926cc0dcae847e29381f
honggoii <46019755+honggoii@users.noreply.github.com>
2021-01-18 16:40:21 +0900

    두 수의 합

---
+++ BOJ/20210118/1.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+int arr[100001];
+
+int main() {
+	int ans = 0;
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+		scanf("%d", &arr[i]);
+	sort(arr, arr + n);
+	int x;
+	scanf("%d", &x);
+
+	int i = 0;
+	int j = n - 1;
+	while (1) {
+		if ((arr[i] + arr[j]) == x) {
+			i++;
+			j--;
+			ans++;
+		}
+		else if ((arr[i] + arr[j] > x)) {
+			j--;
+		}
+		else {
+			i++;
+		}
+
+		if (i >= j)
+			break;
+	}
+	printf("%d", ans);
+	return 0;
+}
