4d47f12d0fe14beed09c641d8404cdfda5cdb90c
honggoii <46019755+honggoii@users.noreply.github.com>
2021-01-19 10:23:14 +0900

    큰 수 만들기

---
+++ Programmers/20210119/1.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <stack>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+stack<int> st;
+
+string solution(string number, int k) {
+    string answer = "";
+    st.push(number[0] - '0');
+    int cnt = number.size() - k;
+    for (int i = 1; i < number.size(); i++) {
+        int num = number[i] - '0';
+        while (!st.empty() && (k > 0) && st.top() < num) {
+            st.pop();
+            k--;
+        }            
+        st.push(num);
+    }
+
+    int i = 0;
+    while (!st.empty()) {
+        answer += to_string(st.top());
+        st.pop();
+    }
+    reverse(answer.begin(), answer.end());
+    answer = answer.substr(0, cnt);
+    return answer;
+}
