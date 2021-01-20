60b7b6a89d84d7c6713419636c4b2f8e83630b76
honggoii <46019755+honggoii@users.noreply.github.com>
2021-01-20 16:31:08 +0900

    최댓값과 최솟값

---
+++ Programmers/20210120/4.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    vector<int> v;
+    char* cptr = new char[s.size()];
+    strcpy(cptr, s.c_str());
+    char* ptr = strtok(cptr, " ");
+    while (ptr != NULL) {
+        v.push_back(stoi(ptr));
+        ptr = strtok(NULL, " ");
+    }
+    sort(v.begin(), v.end());
+    answer += to_string(v[0]) + " " + to_string(v[v.size() - 1]);
+    return answer;
+}
