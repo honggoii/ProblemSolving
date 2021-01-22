b2b1c919d5ba3c765c8d9b7f6b8448fb8ccf01c1
honggoii <46019755+honggoii@users.noreply.github.com>
2021-01-22 12:15:21 +0900

    땅따먹기

---
+++ Programmers/20210122/1.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int dp[100001][4];
+int solution(vector<vector<int>> land)
+{
+    int answer = 0;
+    int n = land.size();
+
+    for (int i = 1; i < n; i++) {
+        land[i][0] = land[i][0] + max(land[i - 1][1], max(land[i - 1][2], land[i - 1][3]));
+        land[i][1] = land[i][1] + max(land[i - 1][0], max(land[i - 1][2], land[i - 1][3]));
+        land[i][2] = land[i][2] + max(land[i - 1][0], max(land[i - 1][1], land[i - 1][3]));
+        land[i][3] = land[i][3] + max(land[i - 1][0], max(land[i - 1][1], land[i - 1][2]));
+    }
+
+    answer = max(land[n - 1][0], max(land[n - 1][1], max(dp[n - 1][2], land[n - 1][3])));
+
+    return answer;
+}
