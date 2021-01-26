825b3e2e2949e1be2af0c2d217cf683b809e2a36
honggoii <46019755+honggoii@users.noreply.github.com>
2021-01-26 21:49:40 +0900

    Create 2212.cpp

---
+++ BOJ/20210126/2212.cpp
@@ -0,0 +1,39 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int arr[10001];
+vector<int> v;
+int ans;
+
+bool cmp(int a, int b) {
+	return a > b;
+}
+
+int main() {
+
+	int n, k;
+	scanf("%d%d", &n, &k);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	sort(arr, arr + n);//오름차순 정렬
+	for (int i = 1; i < n; i++) {
+		v.push_back(arr[i] - arr[i - 1]);
+	}
+	sort(v.begin(), v.end(), cmp);//내림차순 정렬
+
+	if (k <= n) {
+		for (int i = 0; i < k - 1; i++) {
+			v.erase(v.begin());
+		}
+	}
+	
+	for (int i = 0; i < v.size(); i++) {
+		ans += v[i];
+	}
+	printf("%d", ans);
+	return 0;
+}
