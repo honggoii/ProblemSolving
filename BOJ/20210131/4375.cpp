23b77dd4d4aa69f18504808d1ce6621d439735cb
honggoii <46019755+honggoii@users.noreply.github.com>
2021-01-31 22:05:55 +0900

    Create 4375.cpp

---
+++ BOJ/20210131/4375.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+
+int main() {
+	int n;
+	
+	while (scanf("%d", &n) != EOF) {
+		int i = 1;
+		int cnt = 1;
+		while (1) {
+			if (i % n == 0) {
+				printf("%d\n", cnt);
+				break;
+			}
+			else {
+				i = (i % n) * 10 + 1;
+				cnt++;
+			}
+		}
+	}
+	return 0;
+}
