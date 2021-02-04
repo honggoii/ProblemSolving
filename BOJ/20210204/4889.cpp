812fef2a2bdb1e33c26628dd4a0c14fc4281fc4a
honggoii <46019755+honggoii@users.noreply.github.com>
2021-02-04 19:29:40 +0900

    Create 4889.cpp

---
+++ BOJ/20210204/4889.cpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+#include <stack>
+#include <cstring>
+using namespace std;
+
+char s[2001];
+int main() {
+	int idx = 1;
+	while (1) {
+		stack<char> st;
+		memset(s, 0, sizeof(s));
+		scanf("%s", s);
+		int ans = 0;
+		if (s[0] == '-')
+			break; //탈출 조건
+
+		for (int i = 0; s[i]; i++) {
+			if (s[i] == '{') {
+				st.push(s[i]);
+			}
+			else if (s[i] == '}') {
+				if (st.empty() || st.top()=='}') {
+					st.push(s[i]);
+				}
+				else if (st.top()=='{'){
+					st.pop();
+				}
+			}
+		}
+		while(!st.empty()) {
+			char tmp = st.top(); st.pop();
+			if (st.top() == tmp)
+				ans += 1;
+			else
+				ans += 2;
+			st.pop();
+		}
+		printf("%d. %d\n", idx, ans);
+		idx++;
+	}
+	return 0;
+}
