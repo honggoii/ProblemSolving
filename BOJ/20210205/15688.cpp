1be3860050b1e06a16474a9c156566df5574c1c5
honggoii <46019755+honggoii@users.noreply.github.com>
2021-02-05 20:41:58 +0900

    Create 15688.cpp

---
+++ BOJ/20210205/15688.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+int arr[1000001];
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+	sort(arr, arr + n);
+	for (int i = 0; i < n; i++) {
+		printf("%d\n", arr[i]);
+	}
+	return 0;
+}
