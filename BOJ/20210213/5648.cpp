2d4d161f9cb4d6cb2944c19947964670c21dbbf4
honggoii <46019755+honggoii@users.noreply.github.com>
2021-02-13 12:15:09 +0900

    Create 5648.cpp

---
+++ BOJ/20210213/5648.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+vector<string> v;
+vector<long long> ans;
+bool cmp(long long a, long long b) {
+	return a < b;
+}
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		string s;
+		cin >> s;
+		int j = s.length() - 1;
+		while (s[j] == '0') {
+			j--;
+		}
+		s = s.substr(0, j+1);
+		reverse(s.begin(), s.end());
+		ans.push_back(stoll(s));
+	}
+	sort(ans.begin(), ans.end(), cmp);
+	for (int i = 0; i < n; i++)
+		cout << ans[i] << '\n';
+	return 0;
+}
