d1174a325c95c2767cdd01d3a455d0e5ead428bf
honggoii <46019755+honggoii@users.noreply.github.com>
2021-02-17 15:03:30 +0900

    Create 11053.cpp

---
+++ BOJ/20210217/11053.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+int arr[1001];
+int dp[1001];
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &arr[i]);
+	}
+	dp[1] = 1;
+	for (int i = 2; i <= n; i++) {
+		dp[i] = 1;
+		for (int j = 1; j < i; j++) {
+			if (arr[j] < arr[i]) {
+				dp[i] = max(dp[j] + 1,dp[i]);
+			}
+		}
+	}
+	int ans = 0;
+	for (int i = 1; i <= n; i++) {
+		if (ans < dp[i])
+			ans = dp[i];
+	}
+	printf("%d", ans);
+	return 0;
+}
