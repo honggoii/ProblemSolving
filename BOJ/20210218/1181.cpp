33788d743cefdf73ac9b343a78dfd9377c8faae6
honggoii <46019755+honggoii@users.noreply.github.com>
2021-02-18 23:46:54 +0900

    Create 1181.cpp

---
+++ BOJ/20210218/1181.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int n;
+vector<string> v;
+bool cmp(string a, string b) {
+	if (a.length() == b.length()) {
+		return a < b;
+	}
+	else {
+		return a.length() < b.length();
+	}
+}
+int main() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		string tmp;
+		cin >> tmp;
+		v.push_back(tmp);
+	}
+	sort(v.begin(), v.end(), cmp);
+	cout << v[0] << "\n";
+	for (int i = 1; i < n; i++) {
+		if (v[i-1] != v[i])
+			cout << v[i] << "\n";
+	}
+	return 0;
+}
