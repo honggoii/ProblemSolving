811e44984ec4f46b174e52190c1728afb00e5445
honggoii <46019755+honggoii@users.noreply.github.com>
2021-02-21 23:47:33 +0900

    Create 10867.cpp

---
+++ BOJ/20210221/10867.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include <set>
+#include <vector>
+#include <algorithm>
+using namespace std;
+set<int> s;
+vector<int> v;
+int n;
+int main() {
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		int a;
+		scanf("%d", &a);
+		s.insert(a);
+	}
+	for (set<int>::iterator it = s.begin(); it != s.end(); it++) {
+		v.push_back(*it);
+	}
+	sort(v.begin(), v.end());
+	for (int i = 0; i < v.size(); i++)
+		printf("%d ", v[i]);
+	return 0;
+}
