7e6ef6664d17930e8896ca5b548f4046bb185c27
honggoii <46019755+honggoii@users.noreply.github.com>
2021-06-07 18:51:58 +0900

    Update 4.py

--- Programmers/20210607/4.py
+++ Programmers/20210607/4.py
@@ -1,50 +1,52 @@
-def solution(new_id):
-    answer = ''
-    new_id = new_id.lower() # 1. 소문자 치환
-    
-    # 2. 소문자, 숫자, -, _, . 제외 문자 제거 
+def two(new_id):
+    tmp = ''
     alpha = 'abcdefghijklmnopqrstuvwxyz'
     digit = '0123456789'
     characters = '-_.'
-    #characters = "~!@#$%^&*()+=`\|"
-    #new_id = ''.join(x for x in new_id if x not in characters)
     for i in new_id:
         if i in alpha or i in digit or i in characters:
-            answer += i
-    new_id = ""
-    cnt = 0
-    # 3. 마침표 2번 이상 -> 마침표 하나로
-    for i in answer:
-        if i == '.':
-            cnt += 1
-        else:
-            if cnt >= 1:
-                new_id += '.'
-                new_id += i
-                cnt = 0
-            else:
-                new_id += i
-    
-    # 4. 마침표가 처음이나 끝이면 제거
+            tmp += i
+    return tmp
+
+def three(new_id):
+    while '..' in new_id:
+        new_id = new_id.replace('..', '.')
+    return new_id
+
+def four(new_id):
     if len(new_id) != 0 and new_id[0] == '.':
-        new_id = new_id[1:len(new_id)]
-    if len(new_id) != 0 and new_id[len(new_id)-1] == '.':
-        new_id = new_id[0:len(new_id)]
-    
-    # 5. 빈 문자열이면 "a" 대입
+        new_id = new_id[1:]
+    if len(new_id) != 0 and new_id[-1] == '.':
+        new_id = new_id[:-1]
+    return new_id
+
+def five(new_id):
     if len(new_id) == 0:
         new_id += 'a'
-        
-    # 7. 길이가 2자 이하, 길이가 3될 때까지 마지막 문자 반복
-    if len(new_id) <= 2:
-        tmp = new_id[len(new_id)-1]
-        for i in range(len(new_id), 3):
-            new_id += tmp
-    # 6. 길이가 16자 이상, 15개 문자외 제거, 마침표가 맨 끝이면 제거
-    elif len(new_id) >= 16:
-        new_id = new_id[0:15]
-        if new_id[14] == '.':
-            new_id = new_id[:14]
+    return new_id
+
+def six(new_id):
+    new_id = new_id[:15]
+    if new_id[-1] == '.':
+        new_id = new_id[:-1]
+    return new_id
+
+def seven(new_id):
+    while len(new_id) < 3:
+        new_id += new_id[-1]
+    return new_id
+
+def solution(new_id):
+    answer = ''
+    new_id = new_id.lower() # 1. 소문자 치환
+    new_id = two(new_id) # 2. 소문자, 숫자, -, _, . 제외 문자 제거 
+    new_id = three(new_id) # 3. 마침표 2번 이상 -> 마침표 하나로 
+    new_id = four(new_id) # 4. 마침표가 처음이나 끝이면 제거
+    new_id = five(new_id) # 5. 빈 문자열이면 "a" 대입
     
+    if len(new_id) <= 2:
+        new_id = seven(new_id) # 7. 길이가 2자 이하, 길이가 3될 때까지 마지막 문자 반복    
+    elif len(new_id) >= 16:
+        new_id = six(new_id) # 6. 길이가 16자 이상, 15개 문자외 제거, 마침표가 맨 끝이면 제거      
     answer = new_id
     return answer
