92bfe0b93985ebf3d838cfc347d5f9a849f0316c
honggoii <46019755+honggoii@users.noreply.github.com>
2021-06-10 03:34:39 +0900

    크레인 인형뽑기 게임

---
+++ Programmers/20210610/4.py
@@ -0,0 +1,18 @@
+def solution(board, moves):
+    answer = 0
+    basket = []
+
+    for i in moves:
+        for j in range(len(board)):
+            if board[j][i-1] != 0:
+                basket.append(board[j][i-1])
+                board[j][i-1] = 0
+
+                if len(basket) >= 2:
+                    if basket[-1] == basket[-2]:
+                        basket.pop()
+                        basket.pop()
+                        answer += 2
+                break
+
+    return answer
