64e95e9316ba9c50093f23c993dc11eb65f5e7e2
honggoii <46019755+honggoii@users.noreply.github.com>
2021-06-19 16:24:58 +0900

    문자열 내림차순으로 배치하기
    https://rudruddl.tistory.com/448

---
+++ Programmers/Level 1/32.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+
+using namespace std;
+
+bool cmp(char a, char b) {
+    return a > b;
+}
+
+string solution(string s) {
+    string answer = "";
+    sort(s.begin(), s.end(), cmp);
+    answer = s;
+    return answer;
+}
