18758595d537305fb984122e39f981cf9922fbcc
honggoii <46019755+honggoii@users.noreply.github.com>
2021-06-19 16:13:13 +0900

    문자열 내 p와 y의 개수

---
+++ Programmers/Level 1/24.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    int pnum = 0;
+    int ynum = 0;
+    for(int i=0; s[i]; i++) {
+        if (s[i]=='p' || s[i]=='P')
+            pnum++;
+        else if (s[i]=='y' || s[i]=='Y')
+            ynum++;
+    }
+    
+    if(pnum != ynum)
+        answer = false;
+
+    return answer;
+}
