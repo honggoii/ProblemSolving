9d321b6a461b387abbf7e79b51393508abfb16a7
honggoii <46019755+honggoii@users.noreply.github.com>
2021-07-01 22:38:44 +0900

    베스트 앨범

---
+++ Programmers/Level 3/1.py
@@ -0,0 +1,28 @@
+def solution(genres, plays):
+    answer = []
+    genre = {}
+    music = {}
+    for i in range(len(genres)):
+        if genres[i] not in genre:
+            genre[genres[i]] = plays[i]
+            music[genres[i]] = []
+            music[genres[i]].append(i)
+        else:
+            genre[genres[i]] += plays[i]
+            music[genres[i]].append(i)
+    genre = sorted(genre.items(), key=(lambda x: x[1]), reverse=True) # 많이 재생된 장르 순
+
+    for key in genre:
+        cnt = 0
+        tmp = []
+        for i in music[key[0]]:
+            tmp.append((plays[i], i)) # 재생시간, 인덱스
+        tmp.sort(key=lambda x:(-x[0], x[1])) # 재생시간 내림차순, 인덱스 오름차순
+        
+        for t in tmp:
+            cnt += 1
+            answer.append(t[1])
+            if cnt == 2: # 장르별 두 개씩만
+                break
+
+    return answer
