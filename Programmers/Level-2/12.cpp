1a9de6be61680a927682098f8dd9b9cb0bfdb617
honggoii <46019755+honggoii@users.noreply.github.com>
2021-07-09 10:10:41 +0900

    멀쩡한 사각형
    https://rudruddl.tistory.com/484

--- Programmers/Level-2/12.cpp
+++ Programmers/Level-2/12.cpp
@@ -2,6 +2,7 @@ using namespace std;
 
 long long solution(int w, int h) {
     long long answer = 0;
+    
     while ((w != 0) && (h != 0)) {
         long long side = (w >= h) ? h : w;//w와 h중 작은 값으로 한 변의 정사각형을 만든다.
         answer += ((side * side) - side);
