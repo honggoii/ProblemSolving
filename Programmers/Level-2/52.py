dd0261538758dcd3e8688f0d46cdecd3bd11ddc3
honggoii <46019755+honggoii@users.noreply.github.com>
2021-07-12 16:53:34 +0900

    이진 변환 반복하기
    https://rudruddl.tistory.com/626

--- Programmers/Level-2/52.py
+++ Programmers/Level-2/52.py
@@ -1,3 +1,6 @@
+'''
+1번째 방법
+'''
 def solution(s):
     cnt = 0 # 이진 변환 횟수
     zero = 0 # 제거된 모든 0 개수
@@ -6,6 +9,20 @@ def solution(s):
         zero += s.count('0') # 제거된 0 개수 추가
         s = list(filter(lambda x: x != '0', s)) # 모든 0 제거
         s = ''.join(s) # 리스트 -> 문자열
+        s = bin(len(s))[2:]
+        cnt += 1 # 변환 횟수 추가
+    return [cnt, zero]
+
+'''
+2번째 방법
+'''
+def solution(s):
+    cnt = 0 # 이진 변환 횟수
+    zero = 0 # 제거된 모든 0 개수
+    
+    while s != '1':
+        zero += s.count('0') # 제거된 0 개수 추가
+        s = '1' * (len(s) - s.count('0')) # 남은 길이 1로 만들기
         s = bin(len(s))[2:]
         cnt += 1 # 변환 횟수 추가
     return [cnt, zero]
